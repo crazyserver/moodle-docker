4c4a429712ec264190a27a9dd3e05791da1c85f0
Pau Ferrer Ocaña <crazyserver@users.noreply.github.com>
2019-12-23 13:48:14 +0100

    Update config.docker-template.php

--- config.docker-template.php
+++ config.docker-template.php
@@ -82,6 +82,10 @@ $CFG->behat_profiles = array(
 $CFG->behat_faildump_path = '/var/www/behatfaildumps';
 $CFG->behat_increasetimeout = getenv('MOODLE_DOCKER_TIMEOUT_FACTOR');
 
+if (!empty(getenv('MOODLE_DOCKER_BEHAT_IONIC_WWWROOT'))) {
+    $CFG->behat_ionic_wwwroot = getenv('MOODLE_DOCKER_BEHAT_IONIC_WWWROOT');
+}
+
 define('PHPUNIT_LONGTEST', true);
 
 if (getenv('MOODLE_DOCKER_APP')) {
